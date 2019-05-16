b6e3bed35a02f4a958ef32c0a60d8269fdc13b37
bafriesen <friesen.benjamin@gmail.com>
2019-05-16 10:31:59 -0600

    commit

--- js/script.js
+++ js/script.js
@@ -2,3 +2,19 @@
   Author: Benjamin Friesen
   Dept: Marketing and Communications
 */
+
+
+document.addEventListener('DOMContentLoaded', function(event) {
+
+  document.getElementById('card-front-to-back-button').style.visibility = 'visible';
+  document.getElementById('card-back-to-front-button').style.visibility = 'visible';
+
+  document.getElementById('card-front-to-back-button').onclick = function() {
+  document.getElementById('card').classList.toggle('do-flip');
+  };
+
+  document.getElementById('card-back-to-front-button').onclick = function() {
+  document.getElementById('flip-card').classList.toggle('do-flip');
+  };
+
+});
